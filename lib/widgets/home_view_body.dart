c51cdc58e43eda4f569ba6c719132bbb9c2224fc
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-12-10 14:51:09 +0200

    make adaptive layout widget to refactor the code

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_adative_design/widgets/adaptive_layout.dart';
 import 'desktop_layout.dart';
 import 'mobile_layout.dart';
 import 'tablet_layout.dart';
@@ -10,16 +11,10 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsetsGeometry.all(12),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth < 600) {
-            return const MobileLayout();
-          } else if (constraints.maxWidth < 900) {
-            return const TabletLayout();
-          } else {
-            return const DesktopLayout();
-          }
-        },
+      child: AdaptiveLayout(
+        mobileLayout: (context) => const MobileLayout(),
+        tabletLayout: (context) => const TabletLayout(),
+        desktopLayout: (context) => const DesktopLayout(),
       ),
     );
   }
